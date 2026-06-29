20818e7f2e78e867dbd39ccfce73cb4de9f4e99e
Abhishek2333 <abhishekkumar151369@gmail.com>
2026-06-29 14:08:30 +0530

    Create Funcpalindrome.c
    To check palindrome using function.

---
+++ Day11/Day11/Day11/Day11/Day12/Funcpalindrome.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+// Function to check palindrome
+int isPalindrome(int n) {
+    int original = n, reverse = 0, digit;
+
+    while (n > 0) {
+        digit = n % 10;
+        reverse = reverse * 10 + digit;
+        n = n / 10;
+    }
+
+    if (original == reverse)
+        return 1;
+    else
+        return 0;
+}
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (isPalindrome(num))
+        printf("%d is a Palindrome Number.\n", num);
+    else
+        printf("%d is not a Palindrome Number.\n", num);
+
+    return 0;
+}
