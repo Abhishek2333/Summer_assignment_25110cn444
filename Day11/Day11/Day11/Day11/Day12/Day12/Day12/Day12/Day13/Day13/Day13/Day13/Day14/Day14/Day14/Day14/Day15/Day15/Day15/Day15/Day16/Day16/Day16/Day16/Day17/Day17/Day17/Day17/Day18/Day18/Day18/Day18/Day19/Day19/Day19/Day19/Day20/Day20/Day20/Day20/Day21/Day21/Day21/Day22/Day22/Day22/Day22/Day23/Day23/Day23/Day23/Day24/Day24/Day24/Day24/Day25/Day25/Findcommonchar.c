ca7c8e9850e965b19e83696d85ea79ff24205647
Abhishek2333 <abhishekkumar151369@gmail.com>
2026-06-29 15:37:49 +0530

    Create Findcommonchar.c
     to find common characters in a strings.

---
+++ Day11/Day11/Day11/Day11/Day12/Day12/Day12/Day12/Day13/Day13/Day13/Day13/Day14/Day14/Day14/Day14/Day15/Day15/Day15/Day15/Day16/Day16/Day16/Day16/Day17/Day17/Day17/Day17/Day18/Day18/Day18/Day18/Day19/Day19/Day19/Day19/Day20/Day20/Day20/Day20/Day21/Day21/Day21/Day22/Day22/Day22/Day22/Day23/Day23/Day23/Day23/Day24/Day24/Day24/Day24/Day25/Day25/Findcommonchar.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    char str1[100], str2[100];
+    int freq1[256] = {0}, freq2[256] = {0};
+    int i;
+
+    printf("Enter first string: ");
+    gets(str1);   
+
+    printf("Enter second string: ");
+    gets(str2);
+
+    
+    for (i = 0; str1[i] != '\0'; i++) {
+        freq1[(int)str1[i]] = 1;
+    }
+
+    
+    for (i = 0; str2[i] != '\0'; i++) {
+        freq2[(int)str2[i]] = 1;
+    }
+
+    printf("Common characters: ");
+
+    
+    for (i = 0; i < 256; i++) {
+        if (freq1[i] && freq2[i] && i != ' ') {
+            printf("%c ", i);
+        }
+    }
+
+    printf("\n");
+
+    return 0;
+}
