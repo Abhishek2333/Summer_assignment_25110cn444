147ae6d1d9142e3c3bf41c0ef5e7384b4ad2f4a8
Abhishek2333 <abhishekkumar151369@gmail.com>
2026-06-29 15:22:35 +0530

    Create Findfirstrepeatingchar.c
     to find first repeating character in a string.

---
+++ Day11/Day11/Day11/Day11/Day12/Day12/Day12/Day12/Day13/Day13/Day13/Day13/Day14/Day14/Day14/Day14/Day15/Day15/Day15/Day15/Day16/Day16/Day16/Day16/Day17/Day17/Day17/Day17/Day18/Day18/Day18/Day18/Day19/Day19/Day19/Day19/Day20/Day20/Day20/Day20/Day21/Day21/Day21/Day22/Day22/Day22/Day22/Day23/Day23/Findfirstrepeatingchar.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    char str[200];
+    int freq[256] = {0};
+    int i = 0;
+
+    printf("Enter a string: ");
+    gets(str);   
+    while (str[i] != '\0') {
+        freq[(int)str[i]]++;
+        i++;
+    }
+
+    
+    i = 0;
+    while (str[i] != '\0') {
+        if (freq[(int)str[i]] > 1) {
+            printf("First repeating character = %c\n", str[i]);
+            return 0;
+        }
+        i++;
+    }
+
+    printf("No repeating character found.\n");
+
+    return 0;
+}
