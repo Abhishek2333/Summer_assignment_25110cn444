6dacef33a9850ba984cf3d985b1db5ffafa9be81
Abhishek2333 <abhishekkumar151369@gmail.com>
2026-06-29 15:24:40 +0530

    Create Checkanagramstrings.c
     to check anagram strings.

---
+++ Day11/Day11/Day11/Day11/Day12/Day12/Day12/Day12/Day13/Day13/Day13/Day13/Day14/Day14/Day14/Day14/Day15/Day15/Day15/Day15/Day16/Day16/Day16/Day16/Day17/Day17/Day17/Day17/Day18/Day18/Day18/Day18/Day19/Day19/Day19/Day19/Day20/Day20/Day20/Day20/Day21/Day21/Day21/Day22/Day22/Day22/Day22/Day23/Day23/Day23/Checkanagramstrings.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    char str1[100], str2[100];
+    int freq[256] = {0};
+    int i = 0;
+
+    printf("Enter first string: ");
+    gets(str1);   
+
+    printf("Enter second string: ");
+    gets(str2);
+
+    
+    while (str1[i] != '\0') {
+        freq[(int)str1[i]]++;
+        i++;
+    }
+
+    
+    i = 0;
+    while (str2[i] != '\0') {
+        freq[(int)str2[i]]--;
+        i++;
+    }
+
+    
+    for (i = 0; i < 256; i++) {
+        if (freq[i] != 0) {
+            printf("Strings are NOT Anagrams\n");
+            return 0;
+        }
+    }
+
+    printf("Strings are Anagrams\n");
+
+    return 0;
+}
